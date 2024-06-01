cde66f6dd5cf2f5cbe2be2cd2bd1f0e31a70237e
Luigi-45 <lufergape02@hotmail.com>
2024-06-01 18:29:25 -0500

    dato

--- src/styles/theme/imagen.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    paper: {
-      paperChannel: 'rgb(255 255 255)', // Establece el canal de papel como blanco (RGB)
-    },
-  },
-});
